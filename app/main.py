434386fcec3bdca523ba1f0258656f6f3af8741c
Satyam Sharma <satyam17102003@gmail.com>
2025-05-10 15:53:37 +0530

    changed the root route message

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!! This is backend api development environment"}
 
 
 

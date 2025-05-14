aa2b7fd792ec1beba63de16c203f7782784f5883
Satyam Sharma <satyam17102003@gmail.com>
2025-05-15 01:26:31 +0530

    Testing the pytest database working with new technique

--- tests/database.py
+++ tests/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 # Database URL format: 'postgresql://<username>:<password>@<host>/<database_name>'
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.test_database_name}"
 
 # Create the SQLAlchemy engine
 test_engine = create_engine(SQLALCHEMY_DATABASE_URL)

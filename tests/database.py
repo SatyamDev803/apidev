e015b34cae8592a41100e843f43c4fda34192dda
Satyam Sharma <satyam17102003@gmail.com>
2025-05-14 16:48:23 +0530

    Trying to fix issues realted database creation

--- tests/database.py
+++ tests/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 # Database URL format: 'postgresql://<username>:<password>@<host>/<database_name>'
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 # Create the SQLAlchemy engine
 test_engine = create_engine(SQLALCHEMY_DATABASE_URL)

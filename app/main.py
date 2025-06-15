2f205aeb20fe8c08e4f0b11562f4e06e66ac9652
Satyam Sharma <satyam17102003@gmail.com>
2025-06-15 22:33:17 +0530

    added frontend config

--- app/main.py
+++ app/main.py
@@ -10,15 +10,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    # "http://www.google.com",
-    # "http://localhost",
-    # "http://localhost:8080",
-    "*"
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
